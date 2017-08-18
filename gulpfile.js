2f2284b8786f9e3435225cd22e5317f632229f7a
dmytrorevak <dmytro27@gmail.com>
2017-08-18 16:21:18 +0300

    create basic project structure

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    auto = require('gulp-autoprefixer'),
+    pug = require('gulp-pug');
+
+
+gulp.task('sass', function() {
+    gulp.src('./src/sass/main.sass')
+        .pipe(sass())
+        .pipe(auto())
+        .pipe(gulp.dest('build/css'));
+});
+
+
+gulp.task('pug', function() {
+    gulp.src('./src/templates/index.pug')
+        .pipe(pug())
+        .pipe(gulp.dest('build/'));
+});
+
+
+gulp.task('build', ['sass', 'pug']);
+
+
+gulp.task('watch', function() {
+    gulp.watch('src/sass/**/*.sass', ['sass']);
+    gulp.watch('src/templates/**/*.pug', ['pug']);
+});
